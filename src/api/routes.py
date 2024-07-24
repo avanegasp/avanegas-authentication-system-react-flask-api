ddac297c75f8ec6eb5d37c2b4918ecb5664fa8c6
avanegas <arco527@gmail.com>
2024-07-23 21:04:24 -0500

    Add signup route with method POST and works

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -21,3 +22,27 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@api.route("/signup", methods=["POST"])
+def signup():
+    body = request.json
+
+    email = body.get("email", None)
+    password = body.get("password", None)
+
+    if email is None or password is None:
+        return jsonify({"error": "email and password required"}), 400
+    
+    password_hash = generate_password_hash(password)
+
+    if User.query.filter_by(email=email).first() is not None:
+        return jsonify({"error":"Email already taken"}), 400
+    
+    try:
+        new_user = User(email=email, password=password, is_active=True)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message":f"User with the email {new_user.email} created successfully"}), 201
+
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error": f"{error}"}), 500
